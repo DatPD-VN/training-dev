ddf17be8e25e64eac40f71904698c24a4f867974
QuocDev101 <quocnb@vnext.vn>
2024-12-24 13:25:42 +0700

    Create Component Flash Sale Products

---
+++ src/components/flash-sale/type.ts
@@ -0,0 +1,10 @@
+type TFlashSaleProps = {
+  time: number;
+};
+type TUseFlashSale = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+
+export type { TFlashSaleProps, TUseFlashSale };
--- src/const/index.ts
+++ src/const/index.ts
@@ -1,6 +1,7 @@
 import { TImageState } from "../recoil/type";
 
 export const DELAY_DEFAULT = 1000;
+export const FLASH_SALE = 10000;
 export const MAX_SIZE_UPLOAD = 102400;
 export const ALLOWED_EXTENSIONS =
   /(\.jpg|\.jpeg|\.png|\.jfif|\.jif|\.jpe)|\.jfif-tbnl$/i;
